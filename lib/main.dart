0cd3d8a99e62d2887534a24672f36e0a05b5ff31
banku27 <pankajparihar123321@gmail.com>
2022-06-28 15:06:59 +0530

    amazon clone

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const AmazonClone());
+}
+
+class AmazonClone extends StatelessWidget {
+  const AmazonClone({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Amazon Clone",
+      debugShowCheckedModeBanner: false,
+      home: Text('hi pankaj'),
+    );
+  }
+}
