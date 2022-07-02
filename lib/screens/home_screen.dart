5729302209f4ebf546ef5b0d9ce67ee0868bb9ff
banku27 <pankajparihar123321@gmail.com>
2022-07-02 22:52:42 +0530

    home screen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -48,16 +48,16 @@ class _HomeScreenState extends State<HomeScreen> {
                   children: [
                     SimpleProductWidget(
                         url:
-                            "https://m.media-amazon.com/images/I/11uufjN3lYL._SX90_SY90_.png"),
+                            "https://m.media-amazon.com/images/I/11yLyO9f9ZL._SX90_SY90_.png"),
                     SimpleProductWidget(
                         url:
-                            "https://m.media-amazon.com/images/I/115yueUc1aL._SX90_SY90_.png"),
+                            "https://m.media-amazon.com/images/I/11BIyKooluL._SX90_SY90_.png"),
                     SimpleProductWidget(
                         url:
-                            "https://m.media-amazon.com/images/I/01cPTp7SLWL._SX90_SY90_.png"),
+                            "https://m.media-amazon.com/images/I/116KbsvwCRL._SX90_SY90_.png"),
                     SimpleProductWidget(
                         url:
-                            "https://m.media-amazon.com/images/I/11M0jYc-tRL._SX90_SY90_.png")
+                            "https://m.media-amazon.com/images/I/11M0jYc-tRL._SX90_SY90_.png"),
                   ],
                 ),
               ],
