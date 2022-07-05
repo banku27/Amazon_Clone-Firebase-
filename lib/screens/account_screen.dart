337df2540425fc13dd8871c0db49e8e8c3b50e64
banku27 <pankajparihar123321@gmail.com>
2022-07-05 11:57:29 +0530

    account screen

--- lib/screens/account_screen.dart
+++ lib/screens/account_screen.dart
@@ -1,7 +1,10 @@
+import 'package:amazon_clone/utils/color_theme.dart';
+import 'package:amazon_clone/utils/constants.dart';
 import 'package:amazon_clone/widgets/account_screen_app_bar.dart';
 import 'package:amazon_clone/widgets/custom_main_button.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class AccountScreen extends StatefulWidget {
   const AccountScreen({Key? key}) : super(key: key);
@@ -23,19 +26,109 @@ class _AccountScreenState extends State<AccountScreen> {
             width: MediaQuery.of(context).size.width,
             child: Column(
               children: [
-                CustomMainButton(
-                    child: const Text(
-                      "Sign Out",
-                      style: TextStyle(color: Colors.black),
-                    ),
-                    color: Colors.orange,
-                    isLoading: false,
-                    onPressed: () {
-                      FirebaseAuth.instance.signOut();
-                    }),
+                IntroductionWidgetAccountScreen(),
+                SizedBox(
+                  height: 10.h,
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: CustomMainButton(
+                      child: const Text(
+                        "Sign Out",
+                        style: TextStyle(color: Colors.black),
+                      ),
+                      color: Colors.orange,
+                      isLoading: false,
+                      onPressed: () {
+                        FirebaseAuth.instance.signOut();
+                      }),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: CustomMainButton(
+                      child: const Text("Sell",
+                          style: TextStyle(color: Colors.black)),
+                      color: yellowColor,
+                      isLoading: false,
+                      onPressed: () {
+                        // Navigator.push(
+                        //     context,
+                        //     MaterialPageRoute(
+                        //         builder: (context) => const SellScreen()));
+                      }),
+                ),
               ],
             ),
           ),
+        ),
+      ),
+    );
+  }
+}
+
+class IntroductionWidgetAccountScreen extends StatelessWidget {
+  const IntroductionWidgetAccountScreen({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    // UserDetailsModel userDetailsModel =
+    //     Provider.of<UserDetailsProvider>(context).userDetails;
+    return Container(
+      height: kAppBarHeight / 2,
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          colors: backgroundGradient,
+          begin: Alignment.centerLeft,
+          end: Alignment.centerRight,
+        ),
+      ),
+      child: Container(
+        height: kAppBarHeight / 2,
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [Colors.white, Colors.white.withOpacity(0.000000000001)],
+            begin: Alignment.bottomCenter,
+            end: Alignment.topCenter,
+          ),
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 17),
+              child: RichText(
+                text: TextSpan(
+                  children: [
+                    TextSpan(
+                      text: "Hello, ",
+                      style: TextStyle(
+                        color: Colors.grey[800],
+                        fontSize: 26,
+                      ),
+                    ),
+                    TextSpan(
+                      text: "Banku",
+                      style: TextStyle(
+                        color: Colors.grey[800],
+                        fontSize: 26,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            const Padding(
+              padding: EdgeInsets.only(right: 20),
+              child: CircleAvatar(
+                backgroundImage: NetworkImage(
+                  "https://m.media-amazon.com/images/I/116KbsvwCRL._SX90_SY90_.png",
+                ),
+              ),
+            )
+          ],
         ),
       ),
     );
